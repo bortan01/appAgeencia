ff461f9b4bee35408d8f94aa3488a095f936de9a
Boris Ricardo Miranda <brmiranda009@gmail.com>
2020-07-19 20:15:43 -0600

    agregando el swiper

--- lib/src/page/paquetes/ListaPaquetes.dart
+++ lib/src/page/paquetes/ListaPaquetes.dart
@@ -25,7 +25,7 @@ class ListaPaquetes extends StatelessWidget {
         //color: Colors.greenAccent,
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[_swiperTarjetas(context), _footer(context)],
+          children: <Widget>[_swiperTarjetas(context)],
         ),
       ),
     );
@@ -33,9 +33,8 @@ class ListaPaquetes extends StatelessWidget {
 
   _swiperTarjetas(BuildContext context) {
     ///estos son datos quemados
-    List<Paquete> paquetes = getPeliculaInventada();
+    final lista = listaInventada();
     final tamanioPantalla = MediaQuery.of(context).size;
-    print("el tamano es " + tamanioPantalla.height.toString());
 
     return Container(
       //color: Colors.blue,
@@ -45,118 +44,82 @@ class ListaPaquetes extends StatelessWidget {
         itemWidth: tamanioPantalla.width * 0.8,
         itemHeight: tamanioPantalla.height * 0.5,
         itemBuilder: (BuildContext context, int index) {
-          paquetes[index].uniqueId = '${paquetes[index].id}-tarjeta';
           return Container(
             child: Hero(
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20.0),
-                  child: GestureDetector(
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
                     child: GestureDetector(
-                      onTap: () {
-                        final tou = paquetes[index];
-
-                        Navigator.pushNamed(context, 'DetallePaquetes',
-                            arguments: tou);
-                      },
+                      onTap: () {},
                       child: FadeInImage(
-                        image: NetworkImage(paquetes[index].getPosterImg()),
+                        image: NetworkImage(lista[index]['imagen']),
                         placeholder: AssetImage('assets/img/no-image.jpg'),
                         fit: BoxFit.cover,
                       ),
-                    ),
-                  )),
-              tag: paquetes[index].uniqueId,
-            ),
+                    )),
+                tag: lista[index]['posicion']),
           );
         },
-        itemCount: paquetes.length,
+        itemCount: lista.length,
         // pagination: new SwiperPagination(),
         // control: new SwiperControl(),
       ),
     );
   }
-}
 
-Widget _footer(BuildContext context) {
-  List<Paquete> peliculasFicticias = getPeliculaInventada();
-  final tamanioPantalla = MediaQuery.of(context).size;
-  double espacioDisponible = (tamanioPantalla.height) * 0.28;
-  return new Container(
-    ///para que tome todo el espacio
-    width: double.infinity,
+  List listaInventada() {
+    String imagen =
+        'https://ik.imagekit.io/tvlk/xpe-asset/AyJ40ZAo1DOyPyKLZ9c3RGQHTP2oT4ZXW+QmPVVkFQiXFSv42UaHGzSmaSzQ8DO5QIbWPZuF+VkYVRk6gh-Vg4ECbfuQRQ4pHjWJ5Rmbtkk=/9593070784419/Big-Bus-Paris-Hop-On-Hop-Off-Tour-3d13f9f8-f228-4788-8c78-91a144a75431.jpeg?_src=imagekit&tr=q-60,c-at_max,w-720,h-512';
+    List<dynamic> lista = new List<dynamic>();
+    //de momento ocuparemos valores ficticios asi que inventarlos
 
-    child: new Column(
-      // crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          child: new Text("Promociones",
-              style: Theme.of(context).textTheme.subtitle1),
-          padding: EdgeInsets.only(left: 25.0),
-        ),
-        new SizedBox(
-          height: 5.0,
-        ),
-        new HorizontalPaquete(
-            paquetes: peliculasFicticias,
-            especioDisponible: espacioDisponible,
-            siguientePagina: () {})
-      ],
-    ),
-  );
-}
+    lista = [
+      {
+        'posicion': 0,
+        'titulo': "Sedan",
+        'subtitulo':
+            "Una característica clave en un sedán es la seguridad que aporta. Los frenos antibloqueo son la primera línea de defensa ante cualquier accidente. Al momento de la colisión, las bolsas de aire pueden salvar la vida de tus pasajeros.",
+        'imagen': imagen,
+        'superficie': 'Auto Familiar',
+        'distancia': '0 km',
+      },
+      {
+        'posicion': 1,
+        'titulo': "Camionetas",
+        'subtitulo':
+            "Empleado generalmente para el transporte de mercancías, un término que hoy en día se aplica a veces informalmente a distintos tipos de automóviles, en concreto pickups, vehículos todoterreno, furgonetas, monovolúmenes, y familiares.",
+        'imagen': imagen,
+        'superficie': '74,8 millones km²',
+        'distancia': ' 57,91 millones km',
+      },
+      {
+        'posicion': 2,
+        'titulo': "Pickup",
+        'subtitulo':
+            "Empleado generalmente para el transporte de mercancías, y que tiene en su parte trasera una zona de carga descubierta (denominada caja, batea, balde, carrocería, platón, cama o palangana), en la cual se pueden colocar objetos grandes.",
+        'imagen': imagen,
+        'superficie': '460,2 millones km²',
+        'distancia': '108,2 millones km',
+      },
+      {
+        'posicion': 3,
+        'titulo': "Microbus",
+        'subtitulo':
+            "Gran furgoneta que tiene asientos en la parte posterior para los pasajeros y ventanas a los lados.",
+        'imagen': imagen,
+        'superficie': '510,1 millones km²',
+        'distancia': '149,6 millones km',
+      },
+      {
+        'posicion': 4,
+        'titulo': "Minivans",
+        'subtitulo':
+            "Ofrece características aptas para la familia, además de toda la tecnología que se podría desear en un paquete atractivo y a un precio muy conveniente.",
+        'imagen': imagen,
+        'superficie': '144,8 millones km²',
+        'distancia': '227,9 millones km',
+      },
+    ];
 
-List<Paquete> getPeliculaInventada() {
-  String direccion =
-      'https://ik.imagekit.io/tvlk/xpe-asset/AyJ40ZAo1DOyPyKLZ9c3RGQHTP2oT4ZXW+QmPVVkFQiXFSv42UaHGzSmaSzQ8DO5QIbWPZuF+VkYVRk6gh-Vg4ECbfuQRQ4pHjWJ5Rmbtkk=/9593070784419/Big-Bus-Paris-Hop-On-Hop-Off-Tour-3d13f9f8-f228-4788-8c78-91a144a75431.jpeg?_src=imagekit&tr=q-60,c-at_max,w-720,h-512';
-  List<Paquete> peliculasFicticias = new List<Paquete>();
-  //de momento ocuparemos valores ficticios asi que inventarlos
-  peliculasFicticias.add(
-    new Paquete(
-        posterPath: direccion,
-        title: "Macupichu",
-        originalTitle: "Machupichu",
-        backdropPath: direccion,
-        id: 2312323123,
-        incluye: [
-          "Hotel",
-          "Desayuno",
-          "Transporte",
-          "Refrigerio",
-          "Gia Turistico"
-        ]),
-  );
-
-  peliculasFicticias.add(new Paquete(
-    posterPath: direccion,
-    title: "Nicaragua",
-    originalTitle: "Nicaragua",
-    backdropPath: direccion,
-    id: 435345534,
-  ));
-
-  peliculasFicticias.add(new Paquete(
-    posterPath: direccion,
-    title: "Volcan de Conchagua",
-    originalTitle: "Volcan de Conchagua",
-    backdropPath: direccion,
-    id: 3454374523,
-  ));
-
-  peliculasFicticias.add(new Paquete(
-    posterPath: direccion,
-    title: "Panama",
-    originalTitle: "Panama",
-    backdropPath: direccion,
-    id: 3454434523,
-  ));
-
-  peliculasFicticias.add(new Paquete(
-    posterPath: direccion,
-    title: "Costa Rica",
-    originalTitle: "Costa Rica",
-    backdropPath: direccion,
-    id: 34543477523,
-  ));
-
-  return peliculasFicticias;
+    return lista;
+  }
 }
--- lib/src/page/vehiculos/DetalleVehiculos.dart
+++ lib/src/page/vehiculos/DetalleVehiculos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 import 'package:peliculas/src/page/vehiculos/HomeCategoria.dart';
 import 'package:peliculas/src/widget/app_bar_widget.dart';
@@ -29,7 +30,8 @@ class DetalleVehiculos extends StatelessWidget {
                 id: "1000"),
             _botones(context),
             _cuadros(),
-            _adicional()
+            _adicional(),
+            _swiperTarjetas(context),
           ]))
         ],
       ),
@@ -273,6 +275,98 @@ Widget _adicional() {
       ],
     ),
   );
+}
+
+_swiperTarjetas(BuildContext context) {
+  ///estos son datos quemados
+  final lista = listaInventada();
+  final tamanioPantalla = MediaQuery.of(context).size;
+
+  return Container(
+    //color: Colors.blue,
+    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+    child: Swiper(
+      layout: SwiperLayout.STACK,
+      itemWidth: tamanioPantalla.width * 0.8,
+      itemHeight: tamanioPantalla.height * 0.5,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          child: Hero(
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: FadeInImage(
+                      image: NetworkImage(lista[index]['imagen']),
+                      placeholder: AssetImage('assets/img/no-image.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  )),
+              tag: lista[index]['posicion']),
+        );
+      },
+      itemCount: lista.length,
+      // pagination: new SwiperPagination(),
+      // control: new SwiperControl(),
+    ),
+  );
+}
+
+List listaInventada() {
+  String imagen =
+      'https://ik.imagekit.io/tvlk/xpe-asset/AyJ40ZAo1DOyPyKLZ9c3RGQHTP2oT4ZXW+QmPVVkFQiXFSv42UaHGzSmaSzQ8DO5QIbWPZuF+VkYVRk6gh-Vg4ECbfuQRQ4pHjWJ5Rmbtkk=/9593070784419/Big-Bus-Paris-Hop-On-Hop-Off-Tour-3d13f9f8-f228-4788-8c78-91a144a75431.jpeg?_src=imagekit&tr=q-60,c-at_max,w-720,h-512';
+  List<dynamic> lista = new List<dynamic>();
+  //de momento ocuparemos valores ficticios asi que inventarlos
+
+  lista = [
+    {
+      'posicion': 0,
+      'titulo': "Sedan",
+      'subtitulo':
+          "Una característica clave en un sedán es la seguridad que aporta. Los frenos antibloqueo son la primera línea de defensa ante cualquier accidente. Al momento de la colisión, las bolsas de aire pueden salvar la vida de tus pasajeros.",
+      'imagen': imagen,
+      'superficie': 'Auto Familiar',
+      'distancia': '0 km',
+    },
+    {
+      'posicion': 1,
+      'titulo': "Camionetas",
+      'subtitulo':
+          "Empleado generalmente para el transporte de mercancías, un término que hoy en día se aplica a veces informalmente a distintos tipos de automóviles, en concreto pickups, vehículos todoterreno, furgonetas, monovolúmenes, y familiares.",
+      'imagen': imagen,
+      'superficie': '74,8 millones km²',
+      'distancia': ' 57,91 millones km',
+    },
+    {
+      'posicion': 2,
+      'titulo': "Pickup",
+      'subtitulo':
+          "Empleado generalmente para el transporte de mercancías, y que tiene en su parte trasera una zona de carga descubierta (denominada caja, batea, balde, carrocería, platón, cama o palangana), en la cual se pueden colocar objetos grandes.",
+      'imagen': imagen,
+      'superficie': '460,2 millones km²',
+      'distancia': '108,2 millones km',
+    },
+    {
+      'posicion': 3,
+      'titulo': "Microbus",
+      'subtitulo':
+          "Gran furgoneta que tiene asientos en la parte posterior para los pasajeros y ventanas a los lados.",
+      'imagen': imagen,
+      'superficie': '510,1 millones km²',
+      'distancia': '149,6 millones km',
+    },
+    {
+      'posicion': 4,
+      'titulo': "Minivans",
+      'subtitulo':
+          "Ofrece características aptas para la familia, además de toda la tecnología que se podría desear en un paquete atractivo y a un precio muy conveniente.",
+      'imagen': imagen,
+      'superficie': '144,8 millones km²',
+      'distancia': '227,9 millones km',
+    },
+  ];
+
+  return lista;
 }
 
 Widget _item({IconData icono, String titulo, String descripcion}) {
