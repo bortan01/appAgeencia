8ea8f23557bd714a46a3f4bbbf9ee0cf4bf3f023
Boris Ricardo Miranda <brmiranda009@gmail.com>
2020-07-24 09:31:25 -0600

    animacion exitosa

--- lib/src/page/inicio/pruebaList.dart
+++ lib/src/page/inicio/pruebaList.dart
@@ -1,63 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:peliculas/src/widget/cource_info_widget.dart';
-import 'modelo/ModeloInformacion.dart';
+import 'package:animate_do/animate_do.dart';
 
-class PruebaList extends StatelessWidget {
+class PruebaList extends StatefulWidget {
   PruebaList({Key key}) : super(key: key);
 
   @override
+  _PruebaListState createState() => _PruebaListState();
+}
+
+class _PruebaListState extends State<PruebaList> {
+  AnimationController animateController;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-            child: Container(
-      child: Column(
-        children: <Widget>[appBarCategorias(), _listado()],
-      ),
+        body: Center(
+            child: ElasticInDown(
+      child: SpinPerfect(
+          // manualTrigger: true,
+
+          //(optional, but mandatory if you use manualTrigger:true) This callback exposes the AnimationController used for the selected animation. Then you can call animationController.forward() to trigger the animation wherever you like manually.
+          controller: (controller) => animateController = controller,
+          child: Square()),
     )));
   }
+}
 
-  Widget appBarCategorias() {
-    return AppBar(
-      backgroundColor: Colors.blue,
-      centerTitle: true,
-      title: Text("Categoría: Sedan"),
-    );
-  }
-
-  Widget _listado() {
-    //Posiblemente esto se convierta en futureBilder
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: _items(CourseList.list),
-        ),
+class Square extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
       ),
     );
-  }
-
-  List<Widget> _items(List<CourseModel> lista) {
-    List<Widget> elementos = [];
-
-    for (var i = 0; i < lista.length; i++) {
-      final elementoTemporal = GestureDetector(
-        onTap: () {
-          print(
-              "tocando el objejto con id " + CourseList.list[i].id.toString());
-        },
-        child: CourceInfoWidget(
-          model: CourseList.list[i],
-        ),
-      );
-      final dividerTempooral = Divider(
-        thickness: 1,
-        endIndent: 20,
-        indent: 20,
-      );
-      elementos.add(elementoTemporal);
-      elementos.add(dividerTempooral);
-    }
-    return elementos;
   }
 }
