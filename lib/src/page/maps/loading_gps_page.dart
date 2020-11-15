4a60cffacfe493bebce290c0b2360d74c62e186c
Boris Ricardo Miranda <brmiranda009@gmail.com>
2020-11-15 17:14:58 -0600

    detectar cuando el usuario activa el gps

--- lib/src/page/maps/loading_gps_page.dart
+++ lib/src/page/maps/loading_gps_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'package:permission_handler/permission_handler.dart';
 import 'package:peliculas/src/page/helpers/helper.dart';
 import 'package:peliculas/src/page/maps/acceso_gps_page.dart';
 import 'package:peliculas/src/page/maps/mapa_page.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
   // const name({Key key}) : super(key: key);
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    ///PARA DETECTAR EL CAMBIO DE LA APLICACION
+    if (state == AppLifecycleState.resumed) {
+      if (await Geolocator.isLocationServiceEnabled()) {
+        Navigator.pushReplacement(
+            context, navegarMapaFadeIn(context, MapaPage()));
+      }
+    }
+    super.didChangeAppLifecycleState(state);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +44,37 @@ class LoadingPage extends StatelessWidget {
       body: FutureBuilder(
         future: this.checkGpsLocatication(context),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return Center(
+          if (snapshot.hasData) {
+            return Center(child: new Text(snapshot.data));
+          } else {
+            return Center(
               child: CircularProgressIndicator(
-            strokeWidth: 10,
-          ));
+                strokeWidth: 8,
+              ),
+            );
+          }
         },
       ),
     );
   }
 
   Future checkGpsLocatication(BuildContext context) async {
-    // PERMISO GPS
-    // GPS ESTA ACTIVO
-    await Future.delayed(new Duration(milliseconds: 1000));
-    print("loading hola mundo ");
-    // Navigator.pushReplacement(context, navegarMapaFadeIn(context, AccesoGpsPage()));
-    // Navigator.pushReplacement(context, navegarMapaFadeIn(context, MapaPage()));
+    //para saver si tengo el permiso
+    final permisoGPS = await Permission.location.isGranted;
+    // PARA DETERMINAR SI TENEMOS PERMISOS GPS
+    final gpsActivo = await Geolocator.isLocationServiceEnabled();
+    if (permisoGPS && gpsActivo) {
+      Navigator.pushReplacement(
+          context, navegarMapaFadeIn(context, MapaPage()));
+      return "";
+
+      ///esto va al snapshop.data
+    } else if (!permisoGPS) {
+      Navigator.pushReplacement(
+          context, navegarMapaFadeIn(context, AccesoGpsPage()));
+      return "Es necesario el permiso de gps";
+    } else {
+      return "Active el GPS";
+    }
   }
 }
