e20fcfc9b13d51a080bf0013895277561acf690f
Boris Ricardo Miranda <brmiranda009@gmail.com>
2021-09-21 18:24:55 -0600

    corrigiendo la reserva de paquetees

--- lib/src/page/tourPaquete/DetallePaquetes.dart
+++ lib/src/page/tourPaquete/DetallePaquetes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/models/tourPaquete/DataTourPaquete_model.dart';
 import 'package:peliculas/src/models/tourPaquete/InformacionAdicional_model.dart';
+import 'package:peliculas/src/models/tourPaquete/historialReserva_model.dart';
 import 'package:peliculas/src/services/turs_services.dart';
 import 'package:peliculas/src/utils/helper.dart';
 import 'package:peliculas/src/widget/app_bar_widget.dart';
@@ -150,17 +151,24 @@ class _DetallePaqueteState extends State<DetallePaquete> {
   }
 
   Widget _crearBoton(BuildContext context) {
+    bool esPaquete =
+        (widget.dataTourPaquete.tipo == 'Paquete Nacional' || widget.dataTourPaquete.tipo == 'Paquete Internacional');
+
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
       child: new RaisedButton.icon(
-        label: new Text("Reservar"),
-        icon: new Icon(Icons.shopping_cart),
+        label: new Text(esPaquete ? "Vistítanos Para Reservar" : "Reservar"),
+        icon: esPaquete ? new Icon(Icons.home) : new Icon(Icons.shopping_cart),
         color: Theme.of(context).accentColor,
         textColor: Theme.of(context).bottomAppBarColor,
         shape: StadiumBorder(),
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => CarritoCompra(dataTourPaquete: widget.dataTourPaquete)));
+          if (esPaquete) {
+            return null;
+          } else {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => CarritoCompra(dataTourPaquete: widget.dataTourPaquete)));
+          }
         },
       ),
     );
